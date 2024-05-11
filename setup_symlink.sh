513849ec716a22af1882fe89f3d751d0443ba572
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2024-05-11 22:25:29 +0200

    Update PS1 with python version

--- setup_symlink.sh
+++ setup_symlink.sh
@@ -1,6 +1,5 @@
 DOTFILES_PATH="$HOME/projects/dotfiles"
 
-ln -sf $DOTFILES_PATH/.gitignore $HOME/.gitignore
 ln -sf $DOTFILES_PATH/.gitconfig $HOME/.gitconfig 
 ln -sf $DOTFILES_PATH/.gitignore_global $HOME/.gitignore_global
 
